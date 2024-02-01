c94fe183ae5c5ed82ca4dd07eef9ca39e5ed69fa
numbo1! <ellenmoltumyr@gmail.com>
2024-02-01 15:00:47 +0100

    ewcc

---
+++ JavaScript-task/README.md
@@ -0,0 +1,2 @@
+# JavaScript-task
+A school project
---
+++ JavaScript-task/forum.js
@@ -0,0 +1,13 @@
+ f = []
+
+function upload(f) {
+    text = document.getElementById("text")
+    person = document.getElementById("person")
+    txt = (text.value)
+    nm = (person.value)
+    document.getElementById("outputs").innerHTML = nm + ": " + txt
+    console.log(outputs)
+    
+    
+}
+
